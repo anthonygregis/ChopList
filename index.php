efcd8310768ac36e1abf400ce5c26fbe717c868b
thedevtron <anthony@thedevtron.com>
2020-02-27 18:27:19 -0500

    Test

--- index.php
+++ index.php
@@ -3,5 +3,16 @@ use voku\helper\HtmlDomParser;
 
 require_once 'vendor/autoload.php';
 
-echo voku\helper\HtmlDomParser::file_get_html('http://nopixel.online/morbrowser/chop-list-database/')->plaintext;
+echo voku\helper\HtmlDomParser::file_get_html('http://nopixel.online/morbrowser/chop-list-database/')->$string;
+
+// Create DOM from string
+$html = str_get_html($string);
+
+$titles = array();
+// Find all tags 
+foreach($html->find($tagname) as $element) {
+    $titles[] = $element->plaintext;
+}
+
+echo count($titles);
 ?>
