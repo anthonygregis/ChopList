a9201749935024409ad3d7dd7c4b7e7f89cafd40
thedevtron <anthony@thedevtron.com>
2020-02-27 18:38:08 -0500

    Test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ use voku\helper\HtmlDomParser;
 
 require_once 'vendor/autoload.php';
 
-echo voku\helper\HtmlDomParser::file_get_html('http://nopixel.online/morbrowser/chop-list-database/')->$html;
+voku\helper\HtmlDomParser::file_get_html('http://nopixel.online/morbrowser/chop-list-database/')->$html;
 
 $tagname = "<h3>";
 
