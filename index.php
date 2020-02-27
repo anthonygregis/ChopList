765ab867ecfa2d3b5a753a036d4906262956b63c
thedevtron <anthony@thedevtron.com>
2020-02-27 18:28:26 -0500

    Test

--- index.php
+++ index.php
@@ -3,10 +3,7 @@ use voku\helper\HtmlDomParser;
 
 require_once 'vendor/autoload.php';
 
-echo voku\helper\HtmlDomParser::file_get_html('http://nopixel.online/morbrowser/chop-list-database/')->$string;
-
-// Create DOM from string
-$html = str_get_html($string);
+echo voku\helper\HtmlDomParser::file_get_html('http://nopixel.online/morbrowser/chop-list-database/')->$html;
 
 $titles = array();
 // Find all tags 
