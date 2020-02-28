6bb9c848c1dc00ac286de6ec1b4c8b264525f7ec
thedevtron <anthony@thedevtron.com>
2020-02-27 20:29:17 -0500

    Test

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+session_start();
 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    header("location: car-result.php?car=".$car);
+}
 ?>
 <html>
   <head>
@@ -11,7 +15,7 @@
   </head>
   <body>
     <div class="s129">
-      <form>
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" >
         <div class="inner-form">
           <div class="input-field">
             <button class="btn-search" type="button">
@@ -19,7 +23,8 @@
                 <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
               </svg>
             </button>
-            <input id="search" type="text" placeholder="What car are you looking for?" />
+            <input id="search" type="text" placeholder="What car are you looking for?" value="<?php echo $car; ?>/>
+            <input type="submit" style="position: absolute; left: -9999px"/>
           </div>
         </div>
       </form>
