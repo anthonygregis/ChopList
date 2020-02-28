70db20ffa1d378021f4aafeeac1b73cd03d7e93f
thedevtron <anthony@thedevtron.com>
2020-02-27 21:03:01 -0500

    Theme

--- car-result.php
+++ car-result.php
@@ -43,21 +43,89 @@ if(isset($_GET["car"])){
     }
 }
 ?>
-<html>
-    <head>
-        <title>Car Results - <?php echo $_GET["car"]; ?></title>
-    </head>
-    <body>
-    <?php if($valid) : ?>
-        <center>
-            <h1><?php echo $car; ?></h1>
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Car Results - <?php echo $_GET["car"]; ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300" data-aos-easing="slide" data-aos-duration="800" data-aos-delay="0">
+  
+
+  <div id="overlayer" style="display: none;"></div>
+  <div class="loader" style="display: none;">
+    <div class="spinner-border text-primary" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+
+
+  <div class="site-wrap">
+    
+    <div id="sticky-wrapper" class="sticky-wrapper" style="height: 0px;"></div>
+
+  
+     
+    <div class="site-blocks-cover overlay aos-init aos-animate" style="background-image: <?php if($valid) : ?>url(images/source.gif);<?php endif; ?><?php if(!$valid) : ?>url(images/JURASSIC.gif);"<?php endif; ?> data-aos="fade" id="home-section">
+
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+
+          
+          <div class="col-md-8 mt-lg-5 text-center">
+          <?php if($valid) : ?>
             <img src=https://www.igta5.com/images/400x160/<?php echo $picture; ?>>
-        </center>
-    <?php endif; ?>
-    <?php if(!$valid) : ?>
-        <center>
-            <h1><?php echo $error; ?></h1>
-        </center>
-    <?php endif; ?>
-    </body>
+            <h1 class="text-uppercase aos-init aos-animate" data-aos="fade-up"><?php echo $car; ?></h1>  
+          <?php endif; ?>
+          <?php if(!$valid) : ?>  
+            <h1 class="text-uppercase aos-init aos-animate" data-aos="fade-up"><?php echo $error; ?></h1>   
+          <?php endif; ?>
+          </div>
+            
+        </div>
+      </div>
+
+      
+    </div>  
+  </div> <!-- .site-wrap -->
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+
+  
+  <script src="js/main.js"></script>
+    
+  
+</body>
 </html>
